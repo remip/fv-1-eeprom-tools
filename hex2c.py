85e53e2deefc4c2d43d88c248af22049b12fce27
Rémi Pauchet <remi.pauchet@gmail.com>
2020-05-21 01:43:11 +0200

    Add binary format input

--- hex2c.py
+++ hex2c.py
@@ -1,43 +1,57 @@
 #!/usr/bin/env python3
 
+import sys
 import re
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-x", "--hexfile", help="Intel hex file", required=True)
+parser.add_argument("-x", "--hexfile", help="Intel hex file")
+parser.add_argument("-b", "--binfile", help="Binary file")
 parser.add_argument("-p", "--presets", help="Preset numbers", nargs='+', type=int, default=[0, 1, 2, 3, 4, 5, 6, 7])
 args = parser.parse_args()
 
-print("Opening Intel HEX dump file `{}`".format(args.hexfile))
 
-with open(args.hexfile, 'r') as hexfh:
-    lines =  hexfh.readlines()
+if not args.hexfile and not args.binfile:
+    parser.print_usage()
+    sys.exit(1)
 
-# decode intel hex format
-# 1024 * 4 bytes
-# 8 presets -> 512 bytes / 128 lines by presets
-# :04 0000 00 80400011 2B
-#  04 bytes
-#  address 0000 
-#  record type 00 (data)
-#  data 80 40 00 11
-#  checksum
+if args.hexfile:
+    print("Opening Intel HEX dump file `{}`".format(args.hexfile))
 
-pattern = re.compile(r"^:04\w{4}00(\w{2})(\w{2})(\w{2})(\w{2})\w{2}\n$")
+    with open(args.hexfile, 'r') as hexfh:
+        lines =  hexfh.readlines()
 
-data = [[] for i in range(8)]
+    # decode intel hex format
+    # 1024 * 4 bytes
+    # 8 presets -> 512 bytes / 128 lines by presets
+    # :04 0000 00 80400011 2B
+    #  04 bytes
+    #  address 0000
+    #  record type 00 (data)
+    #  data 80 40 00 11
+    #  checksum
 
-for preset in range(8):
-    for j in range(preset*128, (preset+1)*128):
-        match = pattern.search(lines[j])
-        if match:            
-            data[preset].append("0x" + match.group(1))
-            data[preset].append("0x" + match.group(2))
-            data[preset].append("0x" + match.group(3))
-            data[preset].append("0x" + match.group(4))
-        else:
-            print("Warning line not decoded:\n{}".format(lines[j]))
+    pattern = re.compile(r"^:04\w{4}00(\w{2})(\w{2})(\w{2})(\w{2})\w{2}\n$")
 
+    data = [[] for i in range(8)]
+
+    for preset in range(8):
+        for j in range(preset*128, (preset+1)*128):
+            match = pattern.search(lines[j])
+            if match:
+                data[preset].append("0x" + match.group(1))
+                data[preset].append("0x" + match.group(2))
+                data[preset].append("0x" + match.group(3))
+                data[preset].append("0x" + match.group(4))
+            else:
+                print("Warning line not decoded:\n{}".format(lines[j]))
+else:
+    with open(args.binfile, 'rb') as binfh:
+        raw = binfh.read()
+        data = [[] for i in range(8)]
+        for preset in range(8):
+            for i in range(512):
+                data[preset].append("0x{:02X}".format(raw[preset*512 + i]))
 
 for preset in args.presets:
     print("Exporting preset {}\n".format(preset))
@@ -47,8 +61,3 @@ for preset in args.presets:
         a.append(", ".join(data[preset][i*16:(i+1)*16]))
     print(",\n".join(a))
     print("};\n")
-
-
-
-
-
